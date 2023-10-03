a63f200825db162a6b9512392655bbf182e21b9d
bhghdhfh <noahliby@gmail.com>
2023-10-03 03:41:46 -0400

    Fixed recursive function ending in a halted program

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ IMMICH_API_URL = functions.cleanurl(os.environ.get('IMMICH_API_URL', 'default_ap
 IMMICH_API_KEY = os.environ.get('IMMICH_API_KEY', 'default_api_key')
 
 @app.route('/')
-def get_random_image():
+def get_random_image(run=0,error=False):
+    if run==10:
+        return error
     try:
         imagedata = functions.get_photo_data(IMMICH_API_KEY,IMMICH_API_URL)
         image = functions.getfile(IMMICH_API_KEY,IMMICH_API_URL,imagedata['id'])
         return send_file(functions.AddDate(image,imagedata['fileCreatedAt']), mimetype='image/jpeg')
-    except:
-        return get_random_image()
+    except Exception as e:
+        return get_random_image(run+1,e)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug = True)
