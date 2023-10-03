481a67b7aff8e44ff256c6cfae857071fa10d1ac
bhghdhfh <noahliby@gmail.com>
2023-10-03 01:42:57 -0400

    Changed configuration to use environment variables

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,7 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
+ENV IMMICH_API_URL=<API_URL>
+ENV IMMICH_API_KEY=<API_KEY>
+
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -4,16 +4,11 @@ from flask import Flask, send_file
 app = Flask(__name__)
 
 
+IMMICH_API_URL = os.environ.get('IMMICH_API_URL', 'default_api_url')
+IMMICH_API_KEY = os.environ.get('IMMICH_API_KEY', 'default_api_key')
+
 @app.route('/')
 def get_random_image():
-    if not os.path.isfile('conf.yaml'):
-        print("template.yaml not found! Please Read the README!")
-        exit(1)
-    else:
-        with open('conf.yaml', 'r') as f:
-            data = yaml.safe_load(f)
-            IMMICH_API_URL = functions.cleanurl(data['url'])
-            IMMICH_API_KEY = data['api-key']
     imagedata = functions.get_photo_data(IMMICH_API_KEY,IMMICH_API_URL)
     image = functions.getfile(IMMICH_API_KEY,IMMICH_API_URL,imagedata['id'])
     return send_file(io.BytesIO(image), mimetype='image/jpeg')
