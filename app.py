fe2b461e49995466cdb784469a0fbc6b4ace3ba7
bhghdhfh <noahliby@gmail.com>
2023-10-03 01:30:39 -0400

    Functional prototype 1

--- app.py
+++ app.py
@@ -1,48 +1,22 @@
 # Version 1.2 9/13/23
-from flask import Flask, redirect, url_for, request, render_template, send_file
+import requests, os, io, yaml, functions
+from flask import Flask, send_file
 app = Flask(__name__)
-import os
-import random
-import time
-import platform
-import glob
-import functions
-# Change this value to change where it looks for images
-imgdir = "/images"
+
 
 @app.route('/')
-def maint():
-   return render_template('wrong-page.html')
-
-#This only returns .png
-@app.route('/img.png',methods = ['POST', 'GET'])
-def png():
-    rphoto = functions.getoneimages(imgdir,'png')
-    return send_file(str(imgdir + "/" + rphoto), mimetype='image/gif')
-
-#This only returns .jpg
-@app.route('/img.jpg',methods = ['POST', 'GET'])
-def jpg():
-    rphoto = functions.getoneimages(imgdir,'jpg')
-    return send_file(str(imgdir + "/" + rphoto), mimetype='image/gif')
-
-#This only returns .gif 
-@app.route('/img.gif',methods = ['POST', 'GET'])
-def gif():
-    rphoto = functions.getoneimages(imgdir,['gif'])
-    return send_file(str(imgdir + "/" + rphoto), mimetype='image/gif')
-
-#This returns all image types
-@app.route('/img',methods = ['POST', 'GET'])
-def all():
-    rphoto = functions.getoneimages(imgdir,['png','jpg','gif'])
-    return send_file(str(imgdir + "/" + rphoto), mimetype='image/gif')
-
-#This returns both jpg and png
-@app.route('/still',methods = ['POST', 'GET'])
-def still():
-    rphoto = functions.getoneimages(imgdir,['png','jpg'])
-    return send_file(str(imgdir + "/" + rphoto), mimetype='image/gif')
+def get_random_image():
+    if not os.path.isfile('conf.yaml'):
+        print("template.yaml not found! Please Read the README!")
+        exit(1)
+    else:
+        with open('conf.yaml', 'r') as f:
+            data = yaml.safe_load(f)
+            IMMICH_API_URL = functions.cleanurl(data['url'])
+            IMMICH_API_KEY = data['api-key']
+    imagedata = functions.get_photo_data(IMMICH_API_KEY,IMMICH_API_URL)
+    image = functions.getfile(IMMICH_API_KEY,IMMICH_API_URL,imagedata['id'])
+    return send_file(io.BytesIO(image), mimetype='image/jpeg')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug = True)
--- functions.py
+++ functions.py
@@ -1,18 +1,45 @@
-import os
-import random
-import time
-import platform
-import glob
+import requests, io, re
+from flask import Flask, send_file
 
-def getimages(imgdir,types):
-    if type(types) != list:
-        types = [types]
-    os.chdir(imgdir)
-    ls = []
-    for x in types:
-        ls.extend(glob.glob(f"*.{x}")) 
-    return ls
-def getoneimages(imgdir,types):
-    ls = getimages(imgdir,types)
-    rphoto = ls[random.randrange(0,len(ls))]
-    return rphoto
+
+def get_photo_data(api,url):
+    # Make a request to Immich API to get a random image URL
+    headers = {'x-api-key': api, 'Accept': 'application/json'}
+    params = {'count': 1}  # Set count to 1 to get a single random image
+    response = requests.get(f"{url}/asset/random", headers=headers, params=params)
+
+    if response.status_code == 200:
+        assets = response.json()
+        if assets == []:
+            return get_photo_data(api,url)
+        else:
+            return assets[0]
+    else:
+        raise Exception('Response error')
+
+def getfile(api,url,id):
+    # Make a request to Immich API to get a random image URL
+    headers = {'x-api-key': api, 'Accept': 'application/octet-stream'}
+    response = requests.post(f"{url}/asset/download/{id}", headers=headers)
+    if response.status_code == 200:
+        return response.content
+    else:
+        print(f"{url}/asset/download/{id}")
+        print(response)
+        raise EnvironmentError
+
+def cleanurl(url):
+    if url[-1] == '/':
+        url = url[:-1]
+    match = re.match('^.*\/\/', url)
+    if match == None:
+        url = f'https://{url}'
+    elif match.string[match.end()-4] == 's':
+        pass
+    else:
+        print("https is recommended")
+    match = re.match('\/api$', url)
+    if match:
+        url = url[:-4]
+    url = f'{url}/api'
+    return url
